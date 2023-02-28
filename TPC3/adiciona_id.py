92a67a1559244176586e47dc66c4aaccb9b6292d
joaoabreu5 <joaomachadoabreu@hotmail.com>
2023-02-28 22:15:59 +0000

    Adicionada 1ª tarefa do TPC3

---
+++ TPC3/adiciona_id.py
@@ -0,0 +1,14 @@
+import json
+
+f = open("dataset-extra1.json", "r")
+dict_dataset = json.load(f)
+f.close()
+
+lista_pessoas = dict_dataset['pessoas']
+
+for i in range(len(lista_pessoas)):
+    lista_pessoas[i]['id'] = 'p' + str(i)
+    
+f = open("dataset-extra1-plus-id.json", "w")
+json.dump(dict_dataset, f, indent=2)
+f.close()
