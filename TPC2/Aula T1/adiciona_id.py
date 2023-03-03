ce7905ed897240c107a4e1a1f85a2dce26e93a51
joaoabreu5 <joaomachadoabreu@hotmail.com>
2023-03-03 00:01:54 +0000

    TPC Aula T1: Correção do nome do dataset

--- TPC2/Aula T1/adiciona_id.py
+++ TPC2/Aula T1/adiciona_id.py
@@ -9,6 +9,6 @@ lista_pessoas = dict_dataset['pessoas']
 for i in range(len(lista_pessoas)):
     lista_pessoas[i]['id'] = 'p' + str(i)
     
-f = open("dataset-extra1-plus-id.json", "w")
+f = open("dataset-extra1.json", "w")
 json.dump(dict_dataset, f, ensure_ascii=False, indent=2)
 f.close()
